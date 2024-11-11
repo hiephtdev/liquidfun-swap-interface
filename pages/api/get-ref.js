e11f817b601f573c3717ba0f229f9c4747c41bbe
hiephtdev <work.hiepht@gmail.com>
2024-11-11 14:46:18 +0700

    fix db

--- pages/api/get-ref.js
+++ pages/api/get-ref.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   try {
     const { walletAddress } = req.query;
-    const result = await Ref.findOne({ walletAddress });
+    const result = await Ref.findOne({ walletAddress: { $regex: new RegExp(`^${walletAddress}$`, 'i') } });
     console.log('result:', result, walletAddress);
     res.status(200).json({ refParam: result ? result.refParam : null });
   } catch (error) {
